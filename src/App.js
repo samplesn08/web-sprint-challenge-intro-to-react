3e69b5fcb7a1146435d100e8cbb8a64dddcc4102
samplesn08 <samplesn08@gmail.com>
2021-02-19 13:40:19 -0500

    made it pretty, added animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import styled from 'styled-components'
-import Character from './components/Character'
+import styled from 'styled-components';
+import Character from './components/Character';
 import './App.css';
 
 const apiURL = 'https://swapi.dev/api/people/'
@@ -22,10 +22,17 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const Header = styled.h1`
+    animation: fadein 2s;
+    @keyframes fadein {
+      from {opacity:0}
+      to {opacity: 1}
+    } 
+  `;
   const arrayOfChars = Array.from(list)
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <Header key='h1' className="Header">Characters</Header>
       {
         arrayOfChars.map(char => {
           return <Character name={char.name} gender={char.gender} height={char.height}/>
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,16 +1,26 @@
 // Write your Character component here
-import React, { useState, useEffect } from 'react';
-import styled, { isStyledComponent } from 'styled-components';
-import App from '../App'
+import React from 'react';
+import styled from 'styled-components';
+
 
 const H1 = styled.h1`
     color: grey;
     border-bottom: 2px black dashed;
     font-size: 1.4rem;
+    transition: .5s;
+    &:hover {
+        color: crimson;
+    }
 `;
 const Div = styled.div`
     background-color: white;
-    width: 80%auto;
+    margin: 0 40%;
+    box-shadow: 5px 5px 5px #443e3e;
+    transition: .5s ease-in-out;
+    &:hover {
+        transform: scale(1.5);
+        color: blue;
+    }
 `;
 
 
