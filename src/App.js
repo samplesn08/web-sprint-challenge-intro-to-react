4302bcb36ca96b8e4d6b935a23500a81545815ca
samplesn08 <samplesn08@gmail.com>
2021-02-19 13:01:01 -0500

    got components rendering properly

--- src/App.js
+++ src/App.js
@@ -18,14 +18,19 @@ const App = () => {
       .catch(err => {
         console.log(err);
       })
-  })
+  }, [])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const arrayOfChars = Array.from(list)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        arrayOfChars.map(char => {
+          return <Character name={char.name} gender={char.gender} height={char.height}/>
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import styled, { isStyledComponent } from 'styled-components';
+import App from '../App'
+
+const H1 = styled.h1`
+    color: grey;
+    border-bottom: 2px black dashed;
+    font-size: 1.4rem;
+`;
+const Div = styled.div`
+    background-color: white;
+    width: 80%auto;
+`;
+
+
+const Character = ({ name, gender, height }) => {
+    return <Div>
+        <H1>{name}</H1>
+        <p>Height: {height}, Gender: {gender}</p>
+    </Div>
+}
+
+export default Character
