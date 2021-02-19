704c41ef6dd20e6f53d8e72724c541afe4159f8c
samplesn08 <samplesn08@gmail.com>
2021-02-19 11:00:55 -0500

    set up codegrade and made initial api call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components'
+import Character from './components/Character'
 import './App.css';
 
+const apiURL = 'https://swapi.dev/api/people/'
+
 const App = () => {
+  const [list, setList] = useState({})
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  useEffect(() => {
+    axios.get(apiURL)
+      .then(res => {
+        setList(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  })
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
